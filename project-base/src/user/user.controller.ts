e833340a9378142a87a15c7ce2e11b1cee1067a8
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-04-27 17:48:03 -0300

    Controller usado para o chamar o service que é crud

--- project-base/src/user/user.controller.ts
+++ project-base/src/user/user.controller.ts
@@ -2,25 +2,25 @@ import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
-@Controller('users') // Este é o caminho base para as rotas
+@Controller('usuario') 
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // Rota para criar um usuário
+ 
   @Post()
   create(@Body() user: User): Promise<User> {
-    return this.userService.create(user); // Chama o service para criar o usuário
+    return this.userService.create(user); 
   }
 
-  // Rota para obter todos os usuários
+ 
   @Get()
   findAll(): Promise<User[]> {
-    return this.userService.findAll(); // Chama o service para retornar todos os usuários
+    return this.userService.findAll();
   }
 
-  // Rota para obter um usuário por ID
+ 
   @Get(':id')
   findOne(@Param('id') id: number): Promise<User | null> {
-    return this.userService.findOne(id); // Chama o service para retornar um usuário específico
+    return this.userService.findOne(id);
   }
 }
