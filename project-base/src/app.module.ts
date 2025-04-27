e9c5e0a940bc9e45eac19ad103ac82abea56ba14
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-04-27 16:55:50 -0300

    Configurando o ambiente conexão banco de dados com para roteamento usando ORM

--- project-base/src/app.module.ts
+++ project-base/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { User } from './user/user.entity'; // Importe seus models
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', 
+      port: 5432, 
+      username: 'user',
+      password: 'password', 
+      database: 'mydatabase', 
+      entities: [User], 
+      synchronize: true, 
+    }),
+    TypeOrmModule.forFeature([User]), 
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
