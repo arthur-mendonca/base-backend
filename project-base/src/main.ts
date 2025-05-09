0f3099dcb2b5db67532204b0ad8c969312eab2b7
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-05-08 22:32:06 -0300

    Inicialização da Aplicação**: O arquivo `main.ts` é responsável por inicializar a aplicação NestJS, criando uma instância do `AppModule` usando `NestFactory.create(AppModule)`.
    - **Configuração do Swagger**:
      - O Swagger é configurado para fornecer documentação interativa da API.
      - Um `DocumentBuilder` é utilizado para definir as propriedades da documentação, como o título, a descrição e a versão da API.
      - O método `SwaggerModule.createDocument(app, config)` gera a documentação com base na configuração fornecida.
    
    - **Rota de Documentação**: A documentação da API é disponibilizada na rota `/api`, permitindo que desenvolvedores e usuários explorem os endpoints da API de forma interativa.
    
    - **Escuta na Porta**: A aplicação escuta na porta especificada pela variável de ambiente `PORT`, ou na porta padrão 3000, caso a variável não esteja definida.

--- project-base/src/main.ts
+++ project-base/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API for managing voluntários, crianças, etc.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
