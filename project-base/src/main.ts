daafe25ec8c92b498539faf4129166a6a7331a15
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-05-11 22:33:13 -0300

    Configuração do servidor NestJS:
    Inicializa o aplicativo com o módulo principal (AppModule).
    Habilita CORS para permitir requisições de diferentes origens.
    Define um prefixo global para todas as rotas (/api).
    Ativa hooks de desligamento para lidar com eventos de encerramento.
    Validações globais:
    
    Configura um ValidationPipe para validar e transformar os dados de entrada.
    Remove propriedades não validadas e impede propriedades não permitidas.
    Tratamento de exceções:
    
    Aplica um filtro global para capturar e formatar exceções.
    Documentação com Swagger:
    
    Configura a documentação da API com título, descrição e versão.
    Disponibiliza a documentação em /api-docs.
    Inicialização do servidor:
    
    Faz o servidor escutar na porta definida ou na porta 3000.
    Exibe a URL do servidor no console

--- project-base/src/main.ts
+++ project-base/src/main.ts
@@ -2,39 +2,36 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from "@nestjs/common";
-import bodyParser from "body-parser";
 import { HttpExceptionFilter } from './common/http-exception.filter';
+
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    app.use(bodyParser.json());
+
     app.enableCors(); // Habilita CORS para todas as rotas
     app.setGlobalPrefix('api'); // Define um prefixo global para todas as rotas
     app.enableShutdownHooks(); // Habilita os hooks de desligamento
-    app.useGlobalFilters(new HttpExceptionFilter());// Usando os filtros de exceção globais
+    app.useGlobalFilters(new HttpExceptionFilter()); // Usando os filtros de exceção globais
     app.useGlobalPipes(new ValidationPipe({
       whitelist: true, // Remove propriedades não validadas
       forbidNonWhitelisted: true, // Lança erro se propriedades não validadas forem encontradas
       transform: true, // Transforma os dados de entrada para os tipos corretos
     }));
 
-    // Configuração do Swagger
+    // Configuração do Swagger (se necessário)
     const config = new DocumentBuilder()
       .setTitle('API Documentation')
-      .setDescription('API for managing voluntários, crianças, etc.')
+      .setDescription('API for managing resources')
       .setVersion('1.0')
-      .addTag('users') // Exemplo de tag
-      .addBearerAuth() // Se você estiver usando autenticação
       .build();
-
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document); // Configuração do Swagger
+    SwaggerModule.setup('api-docs', app, document);
 
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT || 3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
   } catch (error) {
     console.error('Error during application bootstrap:', error);
-    process.exit(1); 
+    process.exit(1);
   }
 }
 
