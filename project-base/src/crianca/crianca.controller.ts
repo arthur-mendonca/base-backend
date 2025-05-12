406ec956b9938792f89a90f5be81494d5b7c739b
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-05-11 22:07:18 -0300

    Protegendo as rotas do controller

--- project-base/src/crianca/crianca.controller.ts
+++ project-base/src/crianca/crianca.controller.ts
@@ -1,30 +1,32 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { CriancaService } from './crianca.service';
 import { Crianca } from './crianca.entity';
 import { CriancaDto } from './crianca.dto';
+import { JwtAuthGuard } from '../autenticacao/jwt-auth.guard'; // Importe o guard
 @Controller('crianca')
 export class CriancaController {
   constructor(private readonly criancaService: CriancaService) { }
-
+ @UseGuards(JwtAuthGuard)
     @Post()
     async create(@Body() criancaData: CriancaDto): Promise<Crianca> {
       return this.criancaService.create(criancaData);
     }
+     @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(): Promise<CriancaDto[]> {
     return this.criancaService.findAll();
   }
-
+ @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<CriancaDto | null> {
     return this.criancaService.findOne(id);
   }
-
+ @UseGuards(JwtAuthGuard)
   @Put(':id')
   async update(@Param('id') id: number, @Body() crianca: CriancaDto): Promise<CriancaDto | null> {
     return this.criancaService.update(id, crianca);
   }
-
+ @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: number): Promise<void> {
     return this.criancaService.remove(id);
