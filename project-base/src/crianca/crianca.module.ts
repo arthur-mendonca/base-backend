fe5dae5421512b09bc0f82c6bde1f6ffaffe5996
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-05-11 22:39:57 -0300

    Importação do ResponsavelModule pois é necessario visto que possui dependencias no banco de dados

--- project-base/src/crianca/crianca.module.ts
+++ project-base/src/crianca/crianca.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Crianca } from './crianca.entity';
 import { CriancaService } from './crianca.service';
 import { CriancaController } from './crianca.controller';
+import { ResponsavelModule } from 'src/responsavel/responsavel.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Crianca])],
+  imports: [TypeOrmModule.forFeature([Crianca]),ResponsavelModule,],
   providers: [CriancaService],
   controllers: [CriancaController],
 })
