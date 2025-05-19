d1869e3f127a8a45c6823a8c95463bc7101f960c
arthur-mendonca <arthu91@gmail.com>
2025-05-19 17:01:12 -0300

    update app.module e main.ts

--- project-base/src/app.module.ts
+++ project-base/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
