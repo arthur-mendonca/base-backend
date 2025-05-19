2451fddc1749e58ed48b18d2c9278bcb1188bd25
arthur-mendonca <arthu91@gmail.com>
2025-05-19 16:55:02 -0300

    refactor: migrate app module to use Prisma; remove TypeORM configuration and add ConfigModule

--- project-base/src/app.module.ts
+++ project-base/src/app.module.ts
@@ -1,29 +1,23 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsuarioModule } from './user/user.module';
-import { CriancaModule } from './crianca/crianca.module';
-import { ResponsavelModule } from './responsavel/responsavel.module';
-import { VoluntarioModule } from './voluntario/voluntario.module';
-import { ParceiroModule } from './parceiro/parceiro.module';
-import { CestaBasicaModule } from './cesta-basica/cestabasica.module';
-import { FrequenciaModule } from './frequencia/frequencia.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
+import { Module, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PrismaModule } from "./prisma/prisma.module";
+import { UsuarioModule } from "./user/user.module";
+import { CriancaModule } from "./crianca/crianca.module";
+import { ResponsavelModule } from "./responsavel/responsavel.module";
+import { VoluntarioModule } from "./voluntario/voluntario.module";
+import { ParceiroModule } from "./parceiro/parceiro.module";
+import { CestaBasicaModule } from "./cesta-basica/cestabasica.module";
+import { FrequenciaModule } from "./frequencia/frequencia.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'migration',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Carregamento automático das entidades
-      synchronize: true, // Usar apenas em dev, em produção prefira migrações
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    PrismaModule,
     UsuarioModule,
     CriancaModule,
     ResponsavelModule,
