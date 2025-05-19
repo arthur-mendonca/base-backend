bf8dd677f8f59f96852cfa805feaa8b8ae47bac0
arthur-mendonca <arthu91@gmail.com>
2025-05-19 17:44:31 -0300

    refactor: simplify Prettier configuration in ESLint settings

--- project-base/eslint.config.mjs
+++ project-base/eslint.config.mjs
@@ -29,16 +29,7 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
-      "prettier/prettier": [
-        "error",
-        {
-          singleQuote: false,
-          trailingComma: "all",
-          semi: true,
-          printWidth: 80,
-          arrowParens: "avoid",
-        },
-      ],
+      "prettier/prettier": ["error"],
     },
   },
 );
