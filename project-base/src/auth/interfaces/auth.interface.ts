1f332bfe7bad2ca8096e7fd3d407071a526205f6
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-06-08 13:15:46 -0300

    Definição de interfaces para tipagem de autenticação
    Eu implementei várias interfaces para tipar os objetos relacionados à autenticação. A interface User define a estrutura do usuário na aplicação, incluindo campos como id_usuario, email, perfil e outros. A interface JwtPayload define a estrutura do payload do token JWT, que inclui o ID do usuário, email e perfil. A LoginResponse define a estrutura da resposta de login, que inclui o token de acesso e os dados do usuário. Por fim, a RequestWithUser  estende a interface Request para incluir informações do usuário autenticado, facilitando o acesso a esses dados nas rotas protegidas.

--- project-base/src/auth/interfaces/auth.interface.ts
+++ project-base/src/auth/interfaces/auth.interface.ts
@@ -1,3 +1,51 @@
+// interfaces/auth.interfaces.ts
+export interface User {
+  id_usuario: number;
+  email: string;
+  perfil: UserRole;
+  nome?: string;
+  telefone?: string;
+  ativo: boolean;
+  data_cadastro: Date;
+  senha?: string;
+}
+
+export interface JwtPayload {
+  sub: number;  // ID do usuário, usado como subject no token
+  email: string;
+  perfil: UserRole; // Perfil do usuário no token
+}
+
 export interface LoginResponse {
-  accessToken: string;
+  accessToken: string;  // Token JWT de acesso
+  user: { // Dados do usuário retornados no login
+    id: number;
+    email: string;
+    perfil: UserRole;
+    nome?: string;
+  };
+}
+// Interface para uso das informações do usuário na requisição HTTP
+export interface RequestWithUser extends Request {
+  user: {
+    id_usuario: number;
+    email: string;
+    perfil: UserRole; 
+    nome?: string;
+    ativo: boolean;
+    data_cadastro: Date;
+  };
+}
+
+// Enum com os perfis de usuário da aplicação
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  COORDENADOR = 'COORDENADOR',
+  VOLUNTARIO = 'VOLUNTARIO',
+  RESPONSAVEL = 'RESPONSAVEL',
+  USUARIO = "USUARIO",
+}
+// Função para validar se uma string é um valor válido do enum UserRole
+export function isValidUserRole(role: string): role is UserRole {
+  return Object.values(UserRole).includes(role as UserRole);
 }
