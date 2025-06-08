c977c5d831f60deb556ae94959dc105ab1155c8b
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-06-08 14:30:30 -0300

    testando

--- project-base/src/auth/dto/login.dto.ts
+++ project-base/src/auth/dto/login.dto.ts
@@ -1,15 +1,9 @@
-import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class LoginDto {
   @IsEmail({}, { message: 'Email deve ter um formato válido' })
-  @IsNotEmpty({ message: 'Email é obrigatório' })
   email: string;
 
   @IsString({ message: 'Senha é obrigatória' })
-  @MinLength(8, { message: 'Senha deve ter pelo menos 8 caracteres' })
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
-    { message: 'Senha deve conter pelo menos: 1 maiúscula, 1 minúscula, 1 número e 1 caractere especial' }
-  )
   senha: string;
 }
