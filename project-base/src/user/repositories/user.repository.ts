c03b408114628cfca00b931eded57d2290a24258
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-06-08 13:46:36 -0300

    debug

--- project-base/src/user/repositories/user.repository.ts
+++ project-base/src/user/repositories/user.repository.ts
@@ -9,7 +9,11 @@ export class UserRepository {
   constructor(private prisma: PrismaService) {}
 
   async findAll(): Promise<UserEntity[]> {
-    return this.prisma.usuario.findMany();
+   const users = await this.prisma.usuario.findMany();
+    return users.map(user => ({
+      ...user,
+      ativo: 'ativo' in user ? (user as any).ativo : false, // Adiciona a propriedade 'ativo' com um valor padrão se existir
+    }));
   }
 
   async findOne(id: number): Promise<UserEntity> {
@@ -21,32 +25,49 @@ export class UserRepository {
       throw new NotFoundException(`Usuário com ID ${id} não encontrado`);
     }
 
-    return user;
+    return {
+      ...user,
+      ativo: 'ativo' in user ? (user as any).ativo : false, // Adiciona a propriedade 'ativo' com um valor padrão se não existir
+    };
   }
 
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
-    return this.prisma.usuario.create({
+    const user = await this.prisma.usuario.create({
       data: createUserDto,
     });
+    return {
+      ...user,
+      ativo: 'ativo' in user ? (user as any).ativo : false, // Garante que 'ativo' está presente
+    };
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
     // Verificar se o usuário existe
     await this.findOne(id);
 
-    return this.prisma.usuario.update({
+    const user = await this.prisma.usuario.update({
       where: { id_usuario: id },
       data: updateUserDto,
     });
+
+    return {
+      ...user,
+      ativo: 'ativo' in user ? (user as any).ativo : false, // Garante que 'ativo' está presente
+    };
   }
 
   async remove(id: number): Promise<UserEntity> {
     // Verificar se o usuário existe
     await this.findOne(id);
 
-    return this.prisma.usuario.delete({
+    const user = await this.prisma.usuario.delete({
       where: { id_usuario: id },
     });
+
+    return {
+      ...user,
+      ativo: 'ativo' in user ? (user as any).ativo : false, // Garante que 'ativo' está presente
+    };
   }
 
   async findByEmail(email: string): Promise<UserEntity | null> {
@@ -54,6 +75,13 @@ export class UserRepository {
       where: { email },
     });
 
-    return user;
+    if (!user) {
+      return null;
+    }
+
+    return {
+      ...user,
+      ativo: 'ativo' in user ? (user as any).ativo : false, // Garante que 'ativo' está presente
+    };
   }
 }
