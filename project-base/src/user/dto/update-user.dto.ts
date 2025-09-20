f30ef7dd06815605f25191163b8ffbd64d316e55
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-09-20 17:52:21 -0300

    Criei o DTO UpdateUserDto para o nosso endpoint de atualização de usuários. Para não ter que reescrever todos os campos do CreateUserDto, utilizei a função PartialType do NestJS, que de forma inteligente pega todas as propriedades do DTO de criação e as torna opcionais. Isso é muito útil porque permite que o usuário envie apenas os campos que deseja alterar, como só o nome ou só o perfil, sem precisar de enviar todos os dados novamente. Adicionalmente, incluí um novo campo opcional, senha_atual, que será necessário para que possamos verificar a senha antiga do usuário antes de permitir que ele defina uma nova, adicionando uma camada extra de segurança. Assim como no DTO de criação, documentei este novo campo com o @ApiPropertyOptional para o Swagger e adicionei a validação com @IsOptional, garantindo que a nossa API de atualização seja flexível e segura.

--- project-base/src/user/dto/update-user.dto.ts
+++ project-base/src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from "@nestjs/swagger";
+import { ApiPropertyOptional, PartialType } from "@nestjs/swagger";
 import { CreateUserDto } from "./create-user.dto";
+import { IsOptional, IsString } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({
+    description: "Senha atual do usuário. Obrigatória se uma nova senha for fornecida.",
+    example: "senhaAntiga123",
+  })
+  @IsOptional()
+  @IsString()
+  senha_atual?: string;
+}
