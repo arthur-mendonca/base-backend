6063e53fece5a53f83be9eb291a1fbf57aa03ac0
Viniciusrz7 <145623272+Viniciusrz7@users.noreply.github.com>
2025-10-04 11:12:14 -0300

    Criei a classe VoluntarioEntity, que implementa a interface Voluntario gerada pelo @prisma/client. Isso me permitiu definir a estrutura de dados exata para um objeto Voluntário, garantindo que ele inclua todos os campos do modelo Prisma correspondente. Mapeei com precisão cada atributo, como id_voluntario (bigint), nome, cpf, rg, endereco, email, telefone, disponibilidade, area_atuacao (todos string), os booleanos tem_antecedentes, respondeu_questionario, aceitou_termos, e o data_cadastro como Date. O campo url_comprovante foi tratado para permitir string | null, refletindo sua opcionalidade. Essa entidade servirá para tipagem e como base para a representação dos dados de voluntário em outras camadas da aplicação.

--- project-base/src/voluntario/entity/voluntario.entity.ts
+++ project-base/src/voluntario/entity/voluntario.entity.ts
@@ -4,10 +4,15 @@ export class VoluntarioEntity implements Voluntario {
   id_voluntario: bigint;
   nome: string;
   cpf: string;
+  rg: string;
+  endereco: string;
   email: string;
   telefone: string;
   disponibilidade: string;
   area_atuacao: string;
+  tem_antecedentes: boolean;
+  url_comprovante: string | null;
   respondeu_questionario: boolean;
   aceitou_termos: boolean;
+  data_cadastro: Date;
 }
